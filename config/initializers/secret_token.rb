3676570e02dd289ecced0764d62aefcf580d2cbf
Samuel Wecker <wecker.samuel@gmail.com>
2011-12-02 14:34:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Htvtapp::Application.config.secret_token = '28645444621c34c8317542119bf4ea1b2ddd8e00406830c4e8682f6f5660e9e102e74eeb0fe71bc5fc5cde175bd7a00d1b19933a0cf0b08f3611d1678a48550b'
